5eab193f8837a0eff5fd28133b68d62d48b914c4
sarahxb <sarahbuligamarga@gmail.com>
2025-11-20 10:33:27 +0200

    Add an end to end test with some real content, taking in account prefilled parameter values #147 *removed unused import

--- application-confluence-migrator-pro-test/application-confluence-migrator-pro-test-docker/src/test/it/com/xwiki/confluencepro/test/ui/ConfluenceMigratorIT.java
+++ application-confluence-migrator-pro-test/application-confluence-migrator-pro-test-docker/src/test/it/com/xwiki/confluencepro/test/ui/ConfluenceMigratorIT.java
@@ -23,7 +23,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
